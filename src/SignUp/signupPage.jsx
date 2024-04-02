30b0c6744836f054bd05fc5b9966054c9bb37b55
sjsojin <qkrsojin@gmail.com>
2024-04-03 08:34:58 +0900

    리액트 기본 세팅 #2

---
+++ src/SignUp/signupPage.jsx
@@ -0,0 +1,7 @@
+import signup from "/Users/sojin/Desktop/리액트/keep-web/src/SignUp/signup.css";
+import React from "react";
+
+const signupPage = () => {
+  return <div></div>;
+};
+export default signupPage;
