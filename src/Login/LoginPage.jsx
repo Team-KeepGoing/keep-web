62c6c2ffecf6b7017bc857de3563686e28c99875
sjsojin <qkrsojin@gmail.com>
2024-04-04 19:36:48 +0900

    Login 기본 세팅 #3

--- src/Login/LoginPage.jsx
+++ src/Login/LoginPage.jsx
@@ -1,8 +1,44 @@
-import React from "react";
+import React, { useState } from "react";
+import bgimg from "../assets/img/universe.svg";
+import Style from "/Users/sojin/Desktop/리액트/keep-web/src/Login/LoginStyle.css";
 
 const LoginPage = () => {
-  return <div>
-    
-  </div>;
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [emailError, setEmailError] = useState("");
+
+  const handleSubmit = () => {
+    setEmailError("");
+    console.log("Email:", email);
+    console.log("Password:", password);
+  };
+  return (
+    <div className="back">
+      <img className="bgimg" src={bgimg} alt="backgroundimage" />
+      <div className="brandName">KEEP</div>
+      <div>
+        <label id="ema">이메일</label>
+        <input
+          id="email"
+          className="inputbox"
+          type="email"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+        />
+        {emailError && <p className="error-message">{emailError}</p>}
+        <label id="pass">패스워드</label>
+        <input
+          id="password"
+          className="inputbox"
+          type="password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+        />
+      </div>
+      <button className="button" onClick={handleSubmit}>
+        확인
+      </button>
+    </div>
+  );
 };
 export default LoginPage;
