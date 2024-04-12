abccb7ee9a752be10e91e356c7b800e14bdffe73
sjsojin <qkrsojin@gmail.com>
2024-04-12 12:36:51 +0900

    :: 파일 정리
    pages랑 styles 폴더에 정리

---
+++ src/pages/LoginPage.jsx
@@ -0,0 +1,51 @@
+import React, { useState } from "react";
+import bgimg from "assets/img/universe2.svg";
+import bgimg2 from "assets/img/Rectangle 23background2.svg";
+import logoimg from "assets/img/Guideslogo.svg";
+import "styles/LoginStyle.css";
+
+const LoginPage = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [emailError, setEmailError] = useState("");
+
+  const handleSubmit = () => {
+    setEmailError("");
+    console.log("Email:", email);
+    console.log("Password:", password);
+  };
+  return (
+    <div className="back">
+      <img className="bgimg" src={bgimg} alt="backgroundimage" />
+      <img className="bgimg2" src={bgimg2} alt="backgroundimage2" />
+      <img className="logoimg" src={logoimg} alt="logoimg" />
+      <div className="brandName">KEEP</div>
+      <div>
+        <label className="email">이메일</label>
+        <div className="emailFormat">@dgsw.hs.kr 형식</div>
+        <input
+          id="email"
+          className="emailInputBox"
+          type="email"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+        />
+        {emailError && <p className="error-message">{emailError}</p>}
+        <label className="password">비밀번호</label>
+        <input
+          id="password"
+          className="passwordInputBox"
+          type="password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+        />
+      </div>
+      <button className="button" onClick={handleSubmit}>
+        로그인
+      </button>
+      <div className="navigate">비밀번호 찾기 | 회원가입</div>
+      {/* 일단은 뷰만. 이후에 navigate 사용하기 */}
+    </div>
+  );
+};
+export default LoginPage;
---
+++ src/pages/SignupPage.jsx
@@ -0,0 +1,79 @@
+import React, { useState } from "react";
+import bgimg from "assets/img/universe2.svg";
+import bgimg2 from "assets/img/Rectangle 23background2.svg";
+import logoimg from "assets/img/Guideslogo.svg";
+// import "styles/SignupStyle.css";
+
+const SignupPage = () => {
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [isTeacher, setIsTeacher] = useState(false);
+  const [emailError, setEmailError] = useState("");
+
+  // const validateEmail = (email) => {
+  //   const emailRegex = /^([\w.%+-]+)@([\w-]+\.)+([\w]{2,})$/i;
+  //   return emailRegex.test(email);
+  // };
+
+  const handleSubmit = () => {
+    setEmailError("");
+    console.log("Name:", name);
+    console.log("Email:", email);
+    console.log("Password:", password);
+    console.log("Is Teacher:", isTeacher);
+  };
+
+  return (
+    <div className="background">
+      <div>
+        <div className="div"></div>
+        <div>
+          <img className="bgimg" src={bgimg} alt="backgroundimage" />
+          <img className="bgimg2" src={bgimg2} alt="backgroundimage2" />
+        </div>
+        <div className="brandName">KEEP</div>
+        <img src={logoimg} alt="keeplogo" className="logo" />
+        <div className="input">
+          <label className="name">이름</label>
+          <input
+            className="nameInputBox"
+            id="name"
+            type="text"
+            value={name}
+            onChange={(e) => setName(e.target.value)}
+          />
+          <label className="email">이메일</label>
+          <input
+            id="email"
+            className="emailInputBox"
+            type="email"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+          />
+          {emailError && <p className="error-message">{emailError}</p>}
+          <label className="password">비밀번호</label>
+          <input
+            id="password"
+            className="passwordInputBox"
+            type="password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
+          <label className="checktext">교사인가요?</label>
+          <input
+            className="checkBox"
+            type="checkbox"
+            checked={isTeacher}
+            onChange={(e) => setIsTeacher(e.target.checked)}
+          />
+        </div>
+        <button className="button" onClick={handleSubmit}>
+          확인
+        </button>
+      </div>
+    </div>
+  );
+};
+
+export default SignupPage;
