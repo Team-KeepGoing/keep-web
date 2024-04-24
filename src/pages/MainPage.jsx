79cfb04d49d9ebea2b2310ab6894efbeb7e5812f
sjsojin <qkrsojin@gmail.com>
2024-04-24 17:37:55 +0900

    ::main 요소 추가

--- src/pages/MainPage.jsx
+++ src/pages/MainPage.jsx
@@ -1,6 +1,8 @@
 import React from "react";
 import { useNavigate } from "react-router-dom";
 import bar from "assets/img/bar.svg";
+// import logo from "assets/img/Guideslogo.svg";
+import dongrami from "assets/img/Ellipse.svg";
 import division from "assets/img/divisionBar.svg";
 import "styles/MainStyle.css";
 
@@ -17,6 +19,7 @@ const MainPage = () => {
 
   return (
     <div className="background">
+      {/* <img src={logo} alt="logo" className="logo" /> */}
       <img src={bar} alt="bar" className="bar" />
       <img src={division} alt="divisionBar" className="divisionBar" />
       <div className="spanTag">
@@ -28,7 +31,26 @@ const MainPage = () => {
         </span>
       </div>
       <h2>맞춤 서비스</h2>
-      <div className="text">KEEP이 제공하는 맞춤 서비스를 한눈에 확인하세요</div>
+      <div className="text">
+        KEEP이 제공하는 맞춤 서비스를 한눈에 확인하세요
+      </div>
+      <button className="qkfhrkrl"></button>
+      <img src={dongrami} alt="동그라미" className="dongrami1"></img>
+      <p className="text1">손쉽게 학생 정보를 확인하세요.</p>
+      <button className="qkfhrkrl2"></button>
+      <img src={dongrami} alt="동그라미" className="dongrami2"></img>
+      <p className="text2">
+        도서/기기 목록 및 실시간
+        <br />
+        대여 현황을 알 수 있습니다.
+      </p>
+      <button className="qkfhrkrl3"></button>
+      <img src={dongrami} alt="동그라미" className="dongrami3"></img>
+      <p className="text3">
+        학생 정보 및 도서/기기
+        <br />
+        정보를 추가합니다.
+      </p>
     </div>
   );
 };
