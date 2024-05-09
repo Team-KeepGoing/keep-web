8e59bb0e796bfe2d7204a14a9b6289faae5032f6
Woong0406 <siung0406@dgsw.hs.kr>
2024-05-09 20:25:46 +0900

    router 오류 수정

--- src/components/router.jsx
+++ src/components/router.jsx
@@ -1,18 +1,19 @@
-
 import SignupPage from "pages/SignupPage.jsx";
 import LoginPage from "pages/LoginPage.jsx";
 import MainPage from "pages/MainPage.jsx";
+import BookOfficer from "pages/BookOfficer";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 const router = () => {
-  return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/signUp" element={<SignupPage />}></Route>
-        <Route path="/login" element={<LoginPage />}></Route>
-        <Route path="/" element={<MainPage />}></Route>
-        <Route path="/bookOfficer" element={<BookOfficer />}></Route>
-      </Routes>
-    </BrowserRouter>
-  );
+    return (
+        <BrowserRouter>
+            <Routes>
+                <Route path="/signUp" element={<SignupPage />}></Route>
+                <Route path="/signin" element={<LoginPage />}></Route>
+                <Route path="/" element={<MainPage />}></Route>
+                <Route path="/bookOfficer" element={<BookOfficer />}></Route>
+            </Routes>
+        </BrowserRouter>
+    );
 };
 
 export default router;
