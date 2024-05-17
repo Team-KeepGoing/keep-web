62b113db24d1e98e92feaed40e26d570fee18c18
Woong0406 <siung0406@dgsw.hs.kr>
2024-05-17 23:20:21 +0900

    contact jsx 수정

--- src/pages/SignupPage.jsx
+++ src/pages/SignupPage.jsx
@@ -26,85 +26,73 @@ const SignupPage = () => {
       return;
     }
 
-    fetch("http://3.34.2.12:8080/user/signup", {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify({
-        email: email,
-        password: password,
-        name: name,
-        teacher: isTeacher ? "true" : "false",
-      }),
-    })
-      .then((response) => response.json())
-      .then((result) => {
-        if (result.message === "SUCCESS") {
-          alert("회원가입 성공");
-          navigate("/signin");
-        } else {
-          alert("회원가입 실패");
-        }
-      })
-      .catch((error) => {
-        console.error("Error:", error);
-        alert("회원가입 실패");
-      });
+    // 회원가입 요청 로직
+  };
+
+  // 엔터키 입력을 감지하는 함수
+  const handleKeyPress = (e) => {
+    if (e.key === 'Enter') {
+      handleSubmit();
+    }
   };
 
   return (
-    <div className="Signupbackground">
-      <div>
-        <div className="div"></div>
+      <div className="Signupbackground">
         <div>
-          <img className="Signupbgimg" src={bgimg} alt="backgroundimage" />
-          <img className="Signupbgimg2" src={bgimg2} alt="backgroundimage2" />
-        </div>
-        <div className="SignupbrandName">KEEP</div>
-        <img src={logoimg} alt="keeplogo" className="Signuplogo" />
-        <div className="input">
-          <label className="Signupname">이름</label>
-          <input
-            className="SignupnameInputBox"
-            id="name"
-            type="text"
-            value={name}
-            onChange={(e) => setName(e.target.value)}
-          />
-          <label className="Signupemail">이메일</label>
-          <input
-            id="email"
-            className="SignupemailInputBox"
-            type="email"
-            value={email}
-            onChange={(e) => setEmail(e.target.value)}
-          />
-          {emailError && <p className="Signuperror-message">{emailError}</p>}
-          <label className="Signuppassword">비밀번호</label>
-          <input
-            id="password"
-            className="SignuppasswordInputBox"
-            type="password"
-            value={password}
-            onChange={(e) => setPassword(e.target.value)}
-          />
-          <label className="Signupchecktext">교사인가요?</label>
-          <input
-            className="SignupcheckBox"
-            type="checkbox"
-            checked={isTeacher}
-            onChange={(e) => setIsTeacher(e.target.checked)}
-          />
-        </div>
-        <button className="Signupbutton" onClick={handleSubmit}>
-          확인
-        </button>
-        <span className="SignupgoBack" onClick={handleBack}>
+          <div className="div"></div>
+          <div>
+            <img className="Signupbgimg" src={bgimg} alt="backgroundimage" />
+            <img className="Signupbgimg2" src={bgimg2} alt="backgroundimage2" />
+          </div>
+          <div className="SignupbrandName">KEEP</div>
+          <img src={logoimg} alt="keeplogo" className="Signuplogo" />
+          <div className="input">
+            {/* 각 입력 필드에 onKeyPress 이벤트 리스너 추가 */}
+            <label className="Signupname">이름</label>
+            <input
+                className="SignupnameInputBox"
+                id="name"
+                type="text"
+                value={name}
+                onChange={(e) => setName(e.target.value)}
+                onKeyPress={handleKeyPress}
+            />
+            <label className="Signupemail">이메일</label>
+            <input
+                id="email"
+                className="SignupemailInputBox"
+                type="email"
+                value={email}
+                onChange={(e) => setEmail(e.target.value)}
+                onKeyPress={handleKeyPress}
+            />
+            {emailError && <p className="Signuperror-message">{emailError}</p>}
+            <label className="Signuppassword">비밀번호</label>
+            <input
+                id="password"
+                className="SignuppasswordInputBox"
+                type="password"
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
+                onKeyPress={handleKeyPress}
+            />
+            <label className="Signupchecktext">교사인가요?</label>
+            <input
+                className="SignupcheckBox"
+                type="checkbox"
+                checked={isTeacher}
+                onChange={(e) => setIsTeacher(e.target.checked)}
+                onKeyPress={handleKeyPress}
+            />
+          </div>
+          <button className="Signupbutton" onClick={handleSubmit}>
+            확인
+          </button>
+          <span className="SignupgoBack" onClick={handleBack}>
           &lt; 뒤로가기
         </span>
+        </div>
       </div>
-    </div>
   );
 };
 
