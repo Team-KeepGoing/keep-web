5a0c6f06b0f75e0b5c6cefde0d63074dc72d0645
sjsojin <qkrsojin@gmail.com>
2024-06-25 14:10:47 +0900

    feat :: signin 됬을 때만 페이지 이동 router 수정

--- src/components/router.jsx
+++ src/components/router.jsx
@@ -1,4 +1,5 @@
-import { BrowserRouter, Route, Routes, Navigate } from "react-router-dom";
+import { BrowserRouter, Route, Routes, useNavigate } from "react-router-dom";
+import { useContext, useEffect } from "react";
 import SignupPage from "pages/SignupPage.jsx";
 import LoginPage from "pages/LoginPage.jsx";
 import MainPage from "pages/MainPage.jsx";
@@ -10,34 +11,67 @@ import Registration from "pages/Registration";
 import BookEntry from "pages/BookEntry";
 import EditBook from "pages/EditBook";
 import EditDevice from "pages/EditDevice";
-import { useContext } from "react";
 import { AuthContext } from "pages/AuthContext";
 import ViewBook from "pages/ViewBook";
 import ViewDevice from "pages/ViewDevice";
 
 const PrivateRoute = ({ element }) => {
   const { user } = useContext(AuthContext);
+  const navigate = useNavigate();
 
-  return user ? element : <Navigate to="/signin" />;
+  useEffect(() => {
+    if (!user) {
+      alert("로그인 후 이용해주세요.");
+      navigate("/signin");
+    }
+  }, [user, navigate]);
+
+  return user ? element : null;
 };
-
 const Router = () => {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/signup" element={<SignupPage />}></Route>
-        <Route path="/signin" element={<LoginPage />}></Route>
-        <Route path="/" element={<MainPage />}></Route>
-        <Route path="/bookOfficer" element={<BookOfficer />}></Route>
-        <Route path="/device" element={<Device />}></Route>
-        <Route path="/studentInfo" element={<StudentInfo />}></Route>
-        <Route path="/emergency" element={<Emergency />}></Route>
-        <Route path="/deviceRegistration" element={<Registration />}></Route>
-        <Route path="/bookEntry" element={<BookEntry />}></Route>
-        <Route path="/editBook" element={<EditBook />}></Route>
-        <Route path="/editDevice" element={<EditDevice />}></Route>
-        <Route path="/viewBook" element={<ViewBook />}></Route>
-        <Route path="/viewDevice" element={<ViewDevice />}></Route>
+        <Route path="/signup" element={<SignupPage />} />
+        <Route path="/signin" element={<LoginPage />} />
+        <Route path="/" element={<MainPage />} />
+        <Route
+          path="/bookOfficer"
+          element={<PrivateRoute element={<BookOfficer />} />}
+        />
+        <Route path="/device" element={<PrivateRoute element={<Device />} />} />
+        <Route
+          path="/studentInfo"
+          element={<PrivateRoute element={<StudentInfo />} />}
+        />
+        <Route
+          path="/emergency"
+          element={<PrivateRoute element={<Emergency />} />}
+        />
+        <Route
+          path="/deviceRegistration"
+          element={<PrivateRoute element={<Registration />} />}
+        />
+        <Route
+          path="/bookEntry"
+          element={<PrivateRoute element={<BookEntry />} />}
+        />
+        <Route
+          path="/editBook"
+          element={<PrivateRoute element={<EditBook />} />}
+        />
+        <Route
+          path="/editDevice"
+          element={<PrivateRoute element={<EditDevice />} />}
+        />
+        <Route
+          path="/viewBook"
+          element={<PrivateRoute element={<ViewBook />} />}
+        />
+        <Route
+          path="/viewDevice"
+          element={<PrivateRoute element={<ViewDevice />} />}
+        />
       </Routes>
     </BrowserRouter>
   );
