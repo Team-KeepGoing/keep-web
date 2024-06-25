6c7433e7657cda2447b6590cf31a35bf4c6bde2f
sjsojin <qkrsojin@gmail.com>
2024-06-25 14:14:42 +0900

    feat :: token 세션 설정

--- src/pages/AuthContext.js
+++ src/pages/AuthContext.js
@@ -1,3 +1,5 @@
+// AuthContext.jsx
+
 import React, { createContext, useState, useEffect } from "react";
 import Cookies from "js-cookie";
 
@@ -8,7 +10,6 @@ export const AuthProvider = ({ children }) => {
   const [loading, setLoading] = useState(true);
 
   useEffect(() => {
-    // 쿠키에서 토큰을 읽어옴
     const token = Cookies.get("token");
     const name = Cookies.get("name");
 
@@ -21,14 +22,12 @@ export const AuthProvider = ({ children }) => {
 
   const login = (userData) => {
     setUser(userData);
-    // 쿠키에 토큰과 이름을 저장
-    Cookies.set("token", userData.token, { expires: 7 }); // expires는 쿠키의 만료 기간을 설정합니다 (옵션)
-    Cookies.set("name", userData.name); // 기본적으로 세션이 끝날 때 쿠키가 삭제됩니다
+    Cookies.set("token", userData.token, { expires: 7 });
+    Cookies.set("name", userData.name);
   };
 
   const logout = () => {
     setUser(null);
-    // 쿠키에서 토큰과 이름을 제거
     Cookies.remove("token");
     Cookies.remove("name");
   };
