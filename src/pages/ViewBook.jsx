d86897e9cd1d0baf668a68bcbf7f764819fe2aad
sjsojin <qkrsojin@gmail.com>
2024-08-29 19:55:36 +0900

    feat :: view 모달 연결

--- src/pages/ViewBook.jsx
+++ src/pages/ViewBook.jsx
@@ -1,10 +1,10 @@
 import React, { useState, useEffect } from "react";
 import { useNavigate, useLocation } from "react-router-dom";
 import "../styles/ViewBook.css";
-// import MainNavbar from "./MainNavbar";
-import BookOfficer from "./BookOfficer";
+import Modal from "./Modal";
+// import BookOfficer from "./BookOfficer";
 
-const ViewBook = () => {
+const ViewBook = ({ isOpen, onClose }) => {
   const [bookName, setBookName] = useState("");
   const [author, setAuthor] = useState("");
   const [bookDate, setBookDate] = useState("");
@@ -23,7 +23,7 @@ const ViewBook = () => {
   }, [location.state]);
 
   const handleCancel = () => {
-    navigate("/bookOfficer");
+    onClose(); // 모달 닫기
   };
 
   const handleEditBook = () => {
@@ -38,53 +38,54 @@ const ViewBook = () => {
   };
 
   return (
-    <div className="ViewBook">
-      <BookOfficer />
-      <div className="ViewForm">
-        <div className="ViewMent">도서 정보</div>
-        <div className="EntryDetailItem">
-          <label className="ViewbookName">도서 제목</label>
-          <input
-            type="text"
-            value={bookName}
-            onChange={(e) => setBookName(e.target.value)}
-            className="ViewNameInput"
-            readOnly
-          />
+    <Modal isOpen={isOpen} onClose={onClose}>
+      <div className="ViewBook">
+        <div className="ViewForm">
+          <div className="ViewMent">도서 정보</div>
+          <div className="EntryDetailItem">
+            <label className="ViewbookName">도서 제목</label>
+            <input
+              type="text"
+              value={bookName}
+              onChange={(e) => setBookName(e.target.value)}
+              className="ViewNameInput"
+              readOnly
+            />
+          </div>
+          <div className="EntryDetailItem">
+            <label className="ViewAuthor">작가</label>
+            <input
+              type="text"
+              value={author}
+              onChange={(e) => setAuthor(e.target.value)}
+              className="ViewAuthorInput"
+              readOnly
+            />
+          </div>
+          <div className="EntryDetailItem">
+            <label className="ViewbookDate">등록일</label>
+            <input
+              type="text"
+              value={bookDate}
+              onChange={(e) => setBookDate(e.target.value)}
+              className="ViewDateInput"
+              readOnly
+            />
+          </div>
+          <div className="EntryDetailItem">
+            {bookImage && (
+              <img src={bookImage} alt="Book" className="BookImagePreview" />
+            )}
+          </div>
+          <button onClick={handleEditBook} className="SaveButton">
+            수정
+          </button>
+          <button onClick={handleCancel} className="CancelButton">
+            취소
+          </button>
         </div>
-        <div className="EntryDetailItem">
-          <label className="ViewAuthor">작가</label>
-          <input
-            type="text"
-            value={author}
-            onChange={(e) => setAuthor(e.target.value)}
-            className="ViewAuthorInput"
-            readOnly
-          />
-        </div>
-        <div className="EntryDetailItem">
-          <label className="ViewbookDate">등록일</label>
-          <input
-            type="text"
-            value={bookDate}
-            onChange={(e) => setBookDate(e.target.value)}
-            className="ViewDateInput"
-            readOnly
-          />
-        </div>
-        <div className="EntryDetailItem">
-          {bookImage && (
-            <img src={bookImage} alt="Book" className="BookImagePreview" />
-          )}
-        </div>
-        <button onClick={handleEditBook} className="SaveButton">
-          수정
-        </button>
-        <button onClick={handleCancel} className="CancelButton">
-          취소
-        </button>
       </div>
-    </div>
+    </Modal>
   );
 };
 
