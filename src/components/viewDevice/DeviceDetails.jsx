c0e1e0930d6c42a7ec76537caa327cbbc092d8e3
sjsojin <qkrsojin@gmail.com>
2024-10-01 12:11:57 +0900

    add :: deviceDetails 컴포넌트 생성

---
+++ src/components/viewDevice/DeviceDetails.jsx
@@ -0,0 +1,55 @@
+import React from "react";
+import Uproad from "../../assets/img/Upload.svg";
+
+const DeviceDetails = ({
+  deviceName,
+  registrationDate,
+  deviceImage,
+  handleEditDevice,
+  onClose,
+}) => {
+  return (
+    <div className="ViewForm">
+      <div className="ViewMent">기기 정보</div>
+      <div className="ViewDetailItem">
+        <label className="ViewDeviceName">기기명</label>
+        <input
+          type="text"
+          value={deviceName}
+          className="ViewDeviceInput"
+          readOnly
+        />
+      </div>
+      <div className="ViewDetailItem">
+        <label className="ViewRegistrationDate">등록일</label>
+        <input
+          type="text"
+          value={registrationDate}
+          className="DeviceViewDateInput"
+          readOnly
+        />
+      </div>
+      <div className="ViewDetailItem">
+        {deviceImage && (
+          <img
+            src={deviceImage}
+            alt="Device"
+            className="DeviceImagePreview"
+            onError={(e) => {
+              e.target.src = Uproad;
+              console.error("Image failed to load:", deviceImage);
+            }}
+          />
+        )}
+      </div>
+      <button onClick={handleEditDevice} className="SaveButton">
+        수정
+      </button>
+      <button onClick={onClose} className="CancelButton">
+        취소
+      </button>
+    </div>
+  );
+};
+
+export default DeviceDetails;
