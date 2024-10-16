119352bb67b56a5595882e25ceb7c12cae75a457
sjsojin <qkrsojin@gmail.com>
2024-10-16 22:55:55 +0900

    feat :: issueType korean text 변경

--- src/pages/Declaration.jsx
+++ src/pages/Declaration.jsx
@@ -12,6 +12,20 @@ import config from "../config/config.json";
 const formatRegDate = (dateString) =>
   !dateString ? "" : new Date(dateString).toISOString().substring(0, 10);
 
+const issueTypeMap = {
+  COVER_DAMAGE: "표지 손상",
+  PAGE_ISSUE: "페이지 관련 문제",
+  BOOK_BODY_DAMAGE: "책 본체 손상",
+  TEXT_PRINT_ISSUE: "텍스트 및 인쇄 문제",
+  ENVIRONMENTAL_DAMAGE: "환경적 손상",
+  SCREEN_ISSUE: "화면 관련 문제",
+  CONNECTIVITY_ISSUE: "연결성 문제",
+  BATTERY_POWER_ISSUE: "배터리 및 전원 문제",
+  SOUND_ISSUE: "음향 문제",
+  EXTERNAL_DAMAGE: "외부 파손",
+  OTHER: "기타",
+};
+
 const Declaration = () => {
   const [deviceData, setDeviceData] = useState([]);
   const [selectedDevice, setSelectedDevice] = useState(null);
@@ -95,7 +109,7 @@ const Declaration = () => {
                 <td>{index + 1}</td>
                 <td>{device.code}</td>
                 <td>{formatRegDate(device.reportDate)}</td>
-                <td>{device.issueType}</td>
+                <td>{issueTypeMap[device.issueType] || device.issueType}</td>
               </tr>
             ))}
           </tbody>
--- src/pages/ViewDamage.jsx
+++ src/pages/ViewDamage.jsx
@@ -3,6 +3,20 @@ import Modal from "./Modal";
 import config from "../config/config.json";
 import "../styles/ViewDamage.css";
 
+const issueTypeMapping = {
+  COVER_DAMAGE: "표지 손상",
+  PAGE_ISSUE: "페이지 관련 문제",
+  BOOK_BODY_DAMAGE: "책 본체 손상",
+  TEXT_PRINT_ISSUE: "텍스트 및 인쇄 문제",
+  ENVIRONMENTAL_DAMAGE: "환경적 손상",
+  SCREEN_ISSUE: "화면 관련 문제",
+  CONNECTIVITY_ISSUE: "연결성 문제",
+  BATTERY_POWER_ISSUE: "배터리 및 전원 문제",
+  SOUND_ISSUE: "음향 문제",
+  EXTERNAL_DAMAGE: "외부 파손",
+  OTHER: "기타",
+};
+
 const ViewDamage = ({ isOpen, onClose, damage }) => {
   const [deviceName, setDeviceName] = useState("");
   const [reportDate, setReportDate] = useState("");
@@ -14,7 +28,7 @@ const ViewDamage = ({ isOpen, onClose, damage }) => {
       console.log("damage data:", damage);
       setDeviceName(damage.deviceName || damage.code); // deviceName이 없을 경우 code 사용
       setReportDate(formatReportDate(damage.reportDate));
-      setIssueType(damage.issueType);
+      setIssueType(issueTypeMapping[damage.issueType] || damage.issueType); // Mapping issue type
       setDescription(damage.description || "");
     }
   }, [damage]);
@@ -94,8 +108,12 @@ const ViewDamage = ({ isOpen, onClose, damage }) => {
         </div>
 
         <div className="button-group">
-          <button onClick={handleDeleteDamage} className="deleteButton">삭제</button>
-          <button onClick={onClose} className="selectButton">확인</button>
+          <button onClick={handleDeleteDamage} className="deleteButton">
+            삭제
+          </button>
+          <button onClick={onClose} className="selectButton">
+            확인
+          </button>
         </div>
       </div>
     </Modal>
